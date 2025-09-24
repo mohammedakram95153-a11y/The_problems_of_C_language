18d0aa956f87cfae09ea5d5e2dc6a17e73279068
mohammed Akram <mohammedakram95153@gmail.com>
2025-09-24 22:47:59 +0530

    Add files via upload

---
+++ 03_problem3.c
@@ -0,0 +1,11 @@
+// write a C program to find the VOLUME of CYCLINDER
+
+#include <stdio.h>
+
+int main()
+{
+    int r = 6;
+    int height = 5;
+    printf("The volume of cyclinder with radius %d and height %d is %f", r, height, 3.14 * r * r * height);
+    return 0;
+}
